f30b00d239b90ab8654ac58b644d951cd66d64ec
Niks Ungurs <98461127+NiksUnG@users.noreply.github.com>
2022-01-26 12:14:51 +0000

    Pievienoju main.py failu

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+mala=int(input("Ievadi kvadrāta malas garumu lielāku par 5: "))
+laukums=math.pow(mala,2)
+print(laukums)
+mala2=mala+5
+laukums2=math.pow(mala2,2)
+print(laukums2)
+procents=laukums/100
+print(procents)
+atbilde=laukums2/procents-100
+print(round(atbilde))
